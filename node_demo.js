06a610b56f67a90710b6f6afaa6a05fe6211a735
haileyli <hailey@Haileys-MacBook-Air-2.local>
2020-05-01 20:16:30 -0400

    test

---
+++ node_demo.js
@@ -0,0 +1,39 @@
+﻿const md5 = require('md5-node');                                    // 调用接口需要引入MD5模块。安装模块：npm install md5-node --save
+const request = require("request");
+
+const ACCOUNT_SID = 'cdc1aabbf2d445108cc13271eexxxxxx';             // 开发者ACCOUNT_SID，请登录sms.miaodiyun.com 获取
+const AUTH_TOKEN = '5ecbeaa30b6c477cb77c2bedb5xxxxxx';              // 开发者AUTH_TOKEN，请登录sms.miaodiyun.com 获取
+const HOST = 'https://openapi.miaodiyun.com/distributor';           // 接口域名
+const SEND_SMS_URI = '/sendSMS';                                    // 普通短信发送URL
+const to = '186xxxxxxxx';                                           // 发送手机号，多个手机号，用英文逗号隔开
+const templateid = '3284';                                          // 模板ID
+const param = '1234';                                               // 短信变量
+const timestamp = new Date().getTime();                             // 时间戳
+const sig = md5(ACCOUNT_SID + AUTH_TOKEN + timestamp);              // 签名：MD5(ACCOUNT SID + AUTH TOKEN + timestamp)。共32位(小写)
+
+sendSMS(SEND_SMS_URI);
+
+// 短信发送
+function sendSMS(url) {
+    const data = `?accountSid=${ACCOUNT_SID}&to=${to}&templateid=${templateid}&param=${param}&timestamp=${timestamp}&sig=${sig}`;
+    post(url, data);
+}
+
+function post(url, data) {
+    const options = {
+        url: HOST + url + data,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        }
+    };
+    request(options, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log('the response from SMS server is:');
+            console.log(body);
+            console.log('node demo finished');
+        } else {
+            console.log(error);
+        }
+    });
+}
